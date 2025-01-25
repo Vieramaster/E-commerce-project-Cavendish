6460cd80670a22b1d60c97f210c2e190539347fa
martin viera <118699912+Vieramaster@users.noreply.github.com>
2025-01-25 19:37:37 +0000

    :bug:

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -28,13 +28,12 @@ export const Maps = ({ markers, zoomMark }) => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <CenterMap center={center} />
-
-        {markers?.map(({ localName, coordinates }, index) => (
-          <Marker key={index} position={coordinates} >
-            <Popup>{localName}</Popup>
-          </Marker>
-        ))}
+       
+       <Marker position={center}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
       </MapContainer>
     </div>
   );
