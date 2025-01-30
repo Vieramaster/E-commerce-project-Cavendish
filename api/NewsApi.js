9a06db593da936617a3c475a99ff3e307d033133
vieramaster <vieramaster@hotmail.com>
2025-01-30 11:27:00 -0300

    :bug:

--- api/NewsApi.js
+++ api/NewsApi.js
@@ -13,12 +13,11 @@ const allowedOrigins = [
 app.use(
   cors({
     origin: (origin, callback) => {
-     
-      if (!origin || allowedOrigins.includes(origin) || origin.endsWith(".vercel.app")) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
+      !origin ||
+      allowedOrigins.includes(origin) ||
+      origin.endsWith(".vercel.app")
+        ? callback(null, true)
+        : callback(new Error("Not allowed by CORS"));
     },
     methods: ["GET"],
     allowedHeaders: ["Content-Type"],
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
+  root: path.resolve('./src'), // Punto de entrada de tu aplicación
+  publicDir: path.resolve(__dirname, 'public'), // Carpeta de archivos públicos
+  server: {
+    port: 3000, // Puerto de desarrollo
   },
-
+  build: {
+    outDir: path.resolve('dist'), // Carpeta de salida para la construcción
+    emptyOutDir: true, // Limpia la carpeta de salida antes de construir
+  },
 });
