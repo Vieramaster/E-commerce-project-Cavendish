598c4b9fae579eb4d9178a9df3fc950af9e11ac8
vieramaster <vieramaster@hotmail.com>
2025-01-31 12:08:48 -0300

    :sparkles: improving accessibility

--- src/components/lists/SocialBanner.jsx
+++ src/components/lists/SocialBanner.jsx
@@ -4,13 +4,16 @@ export const SocialBanner = () => {
   return (
     <ul className="bg-mainColor w-full h-12 flex justify-center items-center gap-5 lg:h-20">
       {SocialLogosData.map(({ Component, name }) => (
-        <li key={name} className="size-8 lg:size-10">
+        <li
+          key={name}
+          className="size-8 lg:size-10"
+          aria-label={`Visit our ${name} page`}
+        >
           <a
             href="https://www.youtube.com/watch?v=QB7ACr7pUuE"
             target="_blank"
             rel="noopener noreferrer"
             className="stroke-white hover:stroke-yellow duration-300 ease-in-out flex items-center gap-2"
-            aria-label={`Visit our ${name} page`}
           >
             <Component />
             <span className="sr-only">{`Visit our ${name} page`}</span>
